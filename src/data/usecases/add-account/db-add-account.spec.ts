435a5efdd737c37e841e218790f90ec4053732c8
Filipe Macedo <devmacedo@gmail.com>
2020-03-29 17:48:59 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -115,4 +115,22 @@ describe('DbAddAccount Usecase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      ...accountData,
+      password: 'hashed_password',
+      id: 'valid_id'
+    })
+  })
 })
