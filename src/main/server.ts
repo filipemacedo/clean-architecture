bf64905d088dba83fd356122445d9be2d12ba242
Filipe Macedo <devmacedo@gmail.com>
2020-03-30 10:21:32 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,5 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
 const port = 3000
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
